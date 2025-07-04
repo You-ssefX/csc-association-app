d457829f4428913185eb5949742ab52511fcd867
You-ssefX <tazi.y123.0@gmail.com>
2025-07-04 16:02:26 +0200

    Delete frontend directory

--- frontend/config.js
+++
@@ -1,5 +0,0 @@
-import { Platform } from 'react-native';
-
-const BASE_URL = 'http://192.168.1.58:5000'; 
-
-export default BASE_URL;
--- frontend/index.js
+++
@@ -1,5 +0,0 @@
-import { registerRootComponent } from 'expo';
-import App from './App';
-import 'react-native-get-random-values';
-
-registerRootComponent(App);
